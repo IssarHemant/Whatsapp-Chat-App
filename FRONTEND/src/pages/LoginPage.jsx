244aace5c598dd5aca8dfa7913c7c5994e1bdca4
IssarHemant <hemantissar2001@gmail.com>
2025-10-07 15:47:40 +0530

    Adding logo

--- FRONTEND/src/pages/LoginPage.jsx
+++ FRONTEND/src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
                 className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
               transition-colors"
               >
-              <img src="./src/assets/whatsapp.png" alt="" />
+              <img src="./src/Icons/whatsapp.png" alt="" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
               <p className="text-base-content/60">Sign in to your account</p>
--- FRONTEND/src/pages/SignUpPage.jsx
+++ FRONTEND/src/pages/SignUpPage.jsx
@@ -43,7 +43,7 @@ const SignUpPage = () => {
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
               group-hover:bg-primary/20 transition-colors"
               >
-                <img src="./src/assets/whatsapp.png" alt="" />
+                <img src="./src/Icons/whatsapp.png" alt="" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free whatsapp account</p>
